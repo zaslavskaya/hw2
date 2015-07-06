b72c207e785b7b7e40b6d1d3779457c8e1bb6f3f
Anastasiya Zaslavskaya <zaslavskayasya@gmail.com>
2015-07-06 09:43:51 +0300

    correctin: gallery, select, main

---
+++ app/js/main.js
@@ -0,0 +1,75 @@
+//слайдер 
+$(document).ready(function() {
+    var countitem = 0;
+
+    $('.list-portfel li').each(function(index, element) {
+        ++countitem;
+    });
+    $('.list-portfel').css('width', 740 * countitem);
+
+
+    $('#next').click(function() {
+        var sl = $('#portfel').scrollLeft();
+        var newsl = Number(sl) + Number(740);
+        $('#portfel').animate({
+            scrollLeft: newsl
+        }, 'slow');
+    });
+    $('#prev').click(function() {
+        var sl = $('#portfel').scrollLeft();
+        var newsl = sl - 740;
+        $('#portfel').animate({
+            scrollLeft: newsl
+        }, 'slow');
+  
+    });
+
+    //валидация формы плагин 
+
+    $("#myForm").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 3
+            },
+            phone: {
+                required: true,
+                minlength: 8
+            },
+
+            comments: {
+                required: true,
+                minlength: 10
+
+            },
+        },
+        messages: {
+            name: {
+                required: 'Введите имя:',
+                minlength: '> 3-х букв'
+            },
+            phone: {
+                required: 'Введите телефон:',
+                minlength: '> 8-ми цифр'
+            },
+
+            comments: {
+                required: 'Оставьте комментарий!',
+                minlength: 'Напишите больше'
+
+            },
+        },
+
+        success: function(label) {
+
+            label.text('Верно!').addClass('valid');
+
+        },
+
+
+    });
+
+
+
+
+});
